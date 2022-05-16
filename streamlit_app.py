bf10e5a69281d7674553070d67cf8efcf537ed47
chandrikaLakshmi <48353022+chandrikaLakshmi@users.noreply.github.com>
2022-05-16 13:56:03 +0530

    Update streamlit_app.py
    Added new items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My Favourites new healthy diner')
 streamlit.header('Brekfast Menu')
-streamlit.text('Omega3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothiee')
-streamlit.text('Hard boiled free-range egg')
+streamlit.text('🥣 Omega3 & Blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket smoothiee')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
